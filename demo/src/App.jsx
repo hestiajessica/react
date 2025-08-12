d4983ec4c805b55e61359dac1150c9392c0074a2
Hestia Jessica Paul.P <hestiajessica2003@gmail.com>
2025-08-12 14:44:43 +0530

    index

---
+++ demo/src/App.jsx
@@ -0,0 +1,11 @@
+import FunctionComponents  from "./myComponents/intro/functionComponenets"
+
+function App() {
+  return (
+    <div className="App">
+      <FunctionComponents/>
+    </div>
+  )
+}
+
+export default App
---
+++ demo/src/myComponents/intro/functionComponenets.jsx
@@ -0,0 +1,9 @@
+import Reat from 'react'
+let FunctionComponents=()=>{
+    return(
+        <div id="function-section">
+            <p>Hello react, this is my first react project</p>
+        </div>
+    )
+}
+export default FunctionComponents
